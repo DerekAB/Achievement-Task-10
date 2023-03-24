6e2e2260a6b8ab25eefe32264f1ffb17f744a2af
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-03-24 22:59:47 +0000

    added comments and finished

--- Task 10.py
+++ Task 10.py
@@ -11,27 +11,35 @@ class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        
+#Defining the class object attributes
+
     def calculatePerimiter(self):
+        global perimeter
         perimeter = (self.width * 2) + (self.height * 2)
         return perimeter
+        #defining a method that will calculate the perimeter of the rectangle using the object's attributes
         
     def calculateArea(self):
         global area
         area = self.width * self.height
         return area
-    
+        #defining a method that will calculate the area of the rectangle using the object's attributes
+        
     def displayInfo(self):
         print('\nWidth: {} cm'.format(self.width))
         print('Height: {} cm'.format(self.height))
+        print('Perimeter: {} cm'.format(perimeter))
         print('Area: {} cm squared'.format(area))
+        #method that will print out all the information that was input, as well as the perimeter and the area back to the user
         
-rectangle = Rectangle(0, 0)        
+rectangle = Rectangle(10, 12)        
+#defing the variable as an object
 
 rectangle.width = float(input("Please enter the width of the Rectangle (in CM): "))
 rectangle.height = float(input('Please enter the height of the Rectangle (in CM): '))
+#asking the user for the dimensions of the rectangle
 
 Rectangle.calculatePerimiter(rectangle)
 Rectangle.calculateArea(rectangle)
 Rectangle.displayInfo(rectangle)
-
+#calling the methods that will calculate and then print the results to the user
